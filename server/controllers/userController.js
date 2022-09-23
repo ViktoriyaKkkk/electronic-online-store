c4eb7a8709e6231b8042b8fdc5d892193f797205
ViktoriyaKkkk <62558636+ViktoriyaKkkk@users.noreply.github.com>
2022-09-23 23:37:46 +0300

    added routers and controllers

---
+++ server/controllers/userController.js
@@ -0,0 +1,20 @@
+class UserController {
+
+    async registration(req,res){
+
+    }
+
+    async login(req,res){
+
+    }
+
+    async check(req,res){
+    res.json(`jfkd,l`)
+    }
+
+    async delete(req,res){
+
+    }
+}
+
+module.exports = new UserController()
--- server/index.js
+++ server/index.js
@@ -1,4 +1,24 @@
 const express = require(`express`)
-PORT = 5000
+require(`dotenv`).config()
+const  sequelize = require(`./db`)
+const models = require(`./models/models`)
+const cors = require(`cors`)
+const router = require(`./routes/index`)
+
+PORT = process.env.PORT || 5000
+
 const app = express()
-app.listen(PORT, ()=>console.log(`Server started on port ${5000}`))
+app.use(cors())
+app.use(express.json())
+app.use(`/api`, router)
+
+const start = async () => {
+    try{
+        await sequelize.authenticate()
+        await sequelize.sync() //сверяет схему данных с состоянием бд
+        app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+ start()
