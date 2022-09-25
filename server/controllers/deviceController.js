c014d5900cc855fd97c6b7fecd70438a20e7f735
ViktoriyaKkkk <62558636+ViktoriyaKkkk@users.noreply.github.com>
2022-09-25 23:11:08 +0300

    added type creating

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -15,4 +15,5 @@ class DeviceController{
 
     }
 }
+
 module.exports = new DeviceController()
--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -1,10 +1,12 @@
 const Router = require(`express`)
 const router = new Router()
+const deviceController = require(`../controllers/deviceController`)
 
-router.post(`/`,)
-router.get(`/`,)
-router.get(`/:id`,)
-router.delete(`/`,)
+
+router.post(`/`,deviceController.create)
+router.get(`/`,deviceController.getAll)
+router.get(`/:id`,deviceController.getOne)
+router.delete(`/`,deviceController.delete)
 
 
 module.exports = router
